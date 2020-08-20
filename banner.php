a93a00e8b7107aa990887cd8c14397e706998d68
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:44:16 +0200

    Update banner.php

--- banner.php
+++ banner.php
@@ -28,4 +28,22 @@
 						</h3>
 					</div>
 				</div>
-                
+                <div class="carousel-item item4">
+					<div class="carousel-caption">
+						<h3>
+							<?php getbannertext("titles","4") ?>
+						</h3>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+	<!--/model-->
+	<!--//banner-->
