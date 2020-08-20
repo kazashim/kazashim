cd0bbb440ccc0bc5903df0e2e05713c9ef310aea
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:46:12 +0200

    Update search.php

--- search.php
+++ search.php
@@ -11,4 +11,19 @@ require_once('database/db.php');//db config file
 	<meta charset="utf-8">
 	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
 	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
-	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<meta name="keywords" content="<?php getkeywords("titles");?>" /><script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="css/single.css">
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	    rel="stylesheet">
+</head>
