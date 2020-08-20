e9630c02f01cf4389ea05e530251b61c60ccfe1a
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:52:07 +0200

    Update category.php

--- category.php
+++ category.php
@@ -6,3 +6,18 @@ $row = mysqli_fetch_assoc($result);?>
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
+<title>Category-<?php echo $row['name']; ?>|<?php getwebname("titles");?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
+	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
+	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
