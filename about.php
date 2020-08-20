c536af8b17212017c477c8a79c27ed847db05906
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:49:29 +0200

    Update about.php

--- about.php
+++ about.php
@@ -17,3 +17,10 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/single.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	rel="stylesheet">
+</head>
