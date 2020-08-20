e0a9c3401faa860b550a51c800913948a7e3b47b
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:43:35 +0200

    Update banner.php

--- banner.php
+++ banner.php
@@ -6,4 +6,25 @@
 				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-			</ol>
+			</ol><div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<div class="carousel-caption">
+						<h3>
+							<?php getbannertext("titles","1") ?>
+						</h3>
+					</div>
+				</div>
+				<div class="carousel-item item2">
+					<div class="carousel-caption">
+						<h3>
+							<?php getbannertext("titles","2") ?>
+						</h3>
+					</div>
+				</div>
+				<div class="carousel-item item3">
+					<div class="carousel-caption">
+						<h3>
+							<?php getbannertext("titles","3") ?>
+						</h3>
+					</div>
+				</div>
