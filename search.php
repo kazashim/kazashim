1726404e8178be117bd96477982d9936d34b93db
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:46:37 +0200

    Update search.php

--- search.php
+++ search.php
@@ -27,3 +27,26 @@ require_once('database/db.php');//db config file
 	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
 	    rel="stylesheet">
 </head>
+<body>
+	<!--Header-->
+	<?php include("header.php");?>
+	<!--//header-->
+	<?php	
+    $search_keyword = '';
+    if(!empty($_POST['search']['keyword'])) {
+    	$search_keyword = $_POST['search']['keyword'];
+    }
+    $sql = 'SELECT * FROM blogs WHERE title LIKE :keyword OR content LIKE :keyword  OR tags LIKE :keyword OR author LIKE :keyword ORDER BY id DESC ';
+    
+    /* Pagination Code starts */
+    $per_page_html = '';
+    $page = 1;
+    $start=0;
+    if(!empty($_POST["page"])) {
+    	$page = $_POST["page"];
+    	$start=($page-1) * ROW_PER_PAGE;
+    }
+    $limit=" limit " . $start . "," . ROW_PER_PAGE;
+    $pagination_statement = $pdo_conn->prepare($sql);
+    $pagination_statement->bindValue(':keyword', '%' . $search_keyword . '%', PDO::PARAM_STR);
+    $pagination_statement->execute();
