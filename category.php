77146c71217890d4befc79684ff3326f0d1263d1
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:53:42 +0200

    Update category.php

--- category.php
+++ category.php
@@ -125,3 +125,31 @@ $row = mysqli_fetch_assoc($result);?>
 		});
 	</script>
 	<!--// end-smoth-scrolling -->
+    <script>
+		$(document).ready(function () {
+			/*
+									var defaults = {
+							  			containerID: 'toTop', // fading element id
+										containerHoverID: 'toTopHover', // fading element hover id
+										scrollSpeed: 1200,
+										easingType: 'linear' 
+							 		};
+									*/
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<a href="#home" class="scroll" id="toTop" style="display: block;">
+		<span id="toTopHover" style="opacity: 1;"> </span>
+	</a>
+
+	<!-- //Custom-JavaScript-File-Links -->
+	<script src="js/bootstrap.js"></script>
+
+
+</body>
+
+</html>
