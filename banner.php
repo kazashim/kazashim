03317b5f9f45abaea7db79e44de210fa251e17bb
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:43:03 +0200

    Create banner.php

---
+++ banner.php
@@ -0,0 +1,9 @@
+<!--/banner-->
+<div class="banner">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			</ol>
