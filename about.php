3416ee1d3b56efbccf1e430bbad40cd3e1bdb5ac
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:49:45 +0200

    Update about.php

--- about.php
+++ about.php
@@ -24,3 +24,38 @@
 	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
 	rel="stylesheet">
 </head>
+<body>
+	<!--Header-->
+	<?php include("header.php");?>
+	<!--//header-->
+	<!--/banner-->
+	<div class="banner-inner">
+	</div>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item">
+			<a href="index.php">Home</a>
+		</li>
+		<li class="breadcrumb-item active">About</li>
+	</ol>
+	<!--//banner-->
+	<!--/main-->
+	<section class="main-content-w3layouts-agileits">
+		<div class="container">
+			<h3 class="tittle">About</h3>
+			<div class="row inner-sec">
+				<!--left-->
+				<div class="col-lg-8 left-blog-info-w3layouts-agileits text-left">
+					<div class="blog-grid-top">
+
+						<div class="blog_info_left_grid">
+							<a href="single.html">
+								<img src="images/b5.jpg" class="img-fluid" alt="">
+							</a>
+						</div>
+						<h3>
+							<?php getwebname("titles"); echo"|"; gettagline("titles");?>
+						</h3>
+						<p><?php getdetaileddescription("titles");?></p>
+					</div>
+				</div>
+				<!--//left-->
