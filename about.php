dbaf5a610efd09e2d77b4b406dc38634800ac746
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-21 00:49:11 +0200

    Update about.php

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php require("libs/fetch_data.php");?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title><?php getwebname("titles"); echo"|"; gettagline("titles");?>| About</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
+	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
+	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
