e2d0c339a6c12d1e8449d931a67d509e9f39835a
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:47:18 +0200

    Update single.php

--- single.php
+++ single.php
@@ -145,3 +145,26 @@ $roo=mysqli_fetch_assoc($feedback);?>
 													</div>
 												</aside>
 												<!--//right-->
+                                                </div>
+										</div>
+									</section>
+									<!--//main-->
+									<!--footer-->
+									<?php include("footer.php");?>
+									<!---->
+									<!-- js -->
+									<script src="js/jquery-2.2.3.min.js"></script>
+									<!-- //js -->
+									<!--/ start-smoth-scrolling -->
+									<script src="js/move-top.js"></script>
+									<script src="js/easing.js"></script>
+									<script>
+										jQuery(document).ready(function ($) {
+											$(".scroll").click(function (event) {
+												event.preventDefault();
+												$('html,body').animate({
+													scrollTop: $(this.hash).offset().top
+												}, 900);
+											});
+										});
+									</script>
