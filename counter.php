ac6172f6f5b10b668ea6958c75c4f80d46836522
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:53:30 +0200

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,3 +1,13 @@
-<?php require("libs/fetch_data.php");?>
-<!DOCTYPE html>
-<html lang="zxx">
+<?php
+
+// CHECK IF PAGE EXISTS IN PAGE HIT TABLE
+function checkPageName($page_name){
+	$sql = "SELECT * FROM ".$GLOBALS['hits_table_name']." WHERE page = :page";
+	$query = $GLOBALS['db']->prepare($sql);
+	$query->execute([':page' => $page_name]);
+	if ($query->rowCount() == 0){
+		$sql = "INSERT INTO ".$GLOBALS['hits_table_name']." (page, count) VALUES (:page, 0)";
+		$query = $GLOBALS['db']->prepare($sql);
+		$query->execute([':page' => $page_name]);
+	}
+}
