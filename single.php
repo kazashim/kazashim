5f2b93094efb81d41d0cd3285c7c90c82c6f4446
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:45:48 +0200

    Update single.php

--- single.php
+++ single.php
@@ -25,3 +25,12 @@ $roo=mysqli_fetch_assoc($feedback);?>
 			window.scrollTo(0, 1);
 		}
 	</script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="css/single.css">
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	rel="stylesheet">
+	<!--additional javascripts will be placed here-->
+	<?php getjavascripts("links"); ?>
+</head>
