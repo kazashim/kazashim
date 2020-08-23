115f45dac7276f94dff91b89e063b82c50b70fc9
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:45:37 +0200

    Update single.php

--- single.php
+++ single.php
@@ -9,3 +9,19 @@ $count="SELECT * FROM page_hits WHERE page='".$page."'";$feedback=mysqli_query($
 $roo=mysqli_fetch_assoc($feedback);?>
 <!DOCTYPE html>
 <html lang="zxx">
+<head>
+	<title><?php echo $row['title']; ?>|<?php getwebname("titles");?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
+	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
+	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
