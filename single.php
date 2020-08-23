909f34496f0da61ba1a5ca035f80d2d7cbae92c1
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:47:45 +0200

    Update single.php

--- single.php
+++ single.php
@@ -168,3 +168,33 @@ $roo=mysqli_fetch_assoc($feedback);?>
 											});
 										});
 									</script>
+                                    <!--// end-smoth-scrolling -->
+
+									<script>
+										$(document).ready(function () {
+			/*
+									var defaults = {
+							  			containerID: 'toTop', // fading element id
+										containerHoverID: 'toTopHover', // fading element hover id
+										scrollSpeed: 1200,
+										easingType: 'linear' 
+							 		};
+							 		*/
+
+							 		$().UItoTop({
+							 			easingType: 'easeOutQuart'
+							 		});
+
+							 	});
+							 </script>
+							 <a href="#home" class="scroll" id="toTop" style="display: block;">
+							 	<span id="toTopHover" style="opacity: 1;"> </span>
+							 </a>
+
+							 <!-- //Custom-JavaScript-File-Links -->
+							 <script src="js/bootstrap.js"></script>
+
+
+							</body>
+
+							</html>
