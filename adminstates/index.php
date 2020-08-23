98ddd66efeb3a1e808303b45beb2a36533fb870c
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 18:02:40 +0200

    Create index.php

---
+++ adminstates/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+	<title>BLOG ADMIN STATISTICS</title>
+	<link id="browser_favicon" rel="shortcut icon" href="">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="bootstrap/js/jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</head>
