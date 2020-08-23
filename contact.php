47f55732e37594b67d5c08d211b7baeb8eac40d7
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2020-08-23 17:51:25 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -17,3 +17,14 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
+
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link href="css/contact.css" rel='stylesheet' type='text/css' />
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	rel="stylesheet">
+</head>
+
+<body>
+	<!--Header-->
